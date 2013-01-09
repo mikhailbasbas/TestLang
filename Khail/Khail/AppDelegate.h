c494b989f26b1588ec6f0ec66726c43736234f16
MikhailBasbas <mikhail.basbas@gmail.com>
2013-01-09 15:01:17 +0800

    Hehe

---
+++ Khail/Khail/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Khail
+//
+//  Created by Mikhail Lomibao Basbas on 1/9/13.
+//  Copyright (c) 2013 Mikhail Lomibao Basbas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Khail/Khail/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Khail
+//
+//  Created by Mikhail Lomibao Basbas on 1/9/13.
+//  Copyright (c) 2013 Mikhail Lomibao Basbas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
